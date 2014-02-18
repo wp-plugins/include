a49cf107b87ed764a536ee8a6799ba547a4c0fff
mflynn <mflynn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-18 18:55:42 +0000

    Initial Checkin
    git-svn-id: https://plugins.svn.wordpress.org/include/trunk@860406 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include-shortcode.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Include
+ * Plugin URI:
+ * Description:
+ * Version: 1.0
+ * Author: Mike Flynn, C.N.Gann Technology Group, LLC
+ * Author URI: http://cngann.com
+ * License: GPL2
+ */
+
+ /*  [include id="XXX" slug="" show_title="true" title_wrapper="h2" recursion="strict" ]  */
+
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
+	function id_exists($id){ return $wpdb->get_var("SELECT count(*) FROM {$wpdb->posts} WHERE ID = '{$id}'") ? true : false; }
+	$included = array();
+	add_shortcode('include', function($atts){
+		global $included,$wpdb,$post;
+		$included[get_the_ID()] = true;
+		extract( shortcode_atts( array( 'id' => false, 'slug' => false, 'show_title' => false, 'title_wrapper_elem' => 'h2', 'title_wrapper_class' => '', 'recursion' => 'weak' ), $atts, 'include' ) );
+		$st = $show_title; $twe = $title_wrapper_elem; $twc = $title_wrapper_class; $st = $show_title; $re = $recursion;
+		if($id) { if( ! id_exists( $id )) return ""; }
+		else if(!$id && !$slug) return "";
+		else if($slug) $id = $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_name = '{$slug}'");
+		if(!$id) return "";
+		if(empty($included[$id])) $included[$id] = '';
+		if($included[$id] === true) return "";
+		$included[$id] = true;
+		$op = clone $post;
+		$post = get_post($id);
+		setup_postdata($post);
+		$c = $post->post_content;
+		$t = $post->post_title;
+		$r = ($st ? ( $twe ? "<" . $twe . ($twc ? " class=\"" . $twc . "\" " : "") . ">" . $t . "</".$twe.">" : $t ) : "") . do_shortcode( wpautop( (strtolower($re) == "strict" ? preg_replace("/\[include[^\]]*\]/im","",$c) : $c), true ) );
+		$post = clone $op;
+		setup_postdata($post);
+		$included[$id] = false;
+		return $r;
+	});
+
+?>
