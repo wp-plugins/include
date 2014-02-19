1f825fab459c2af0b3bba1dccb40daf90f68a4a9
mflynn <mflynn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-19 13:42:33 +0000

    ver 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/include/tags/1.1.1@860948 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: Include
+ * Plugin URI:http://www.cngann.com/
+ * Description: Include a page, post, activity, or other query-object into another.
+ * Version: 1.1.1
+ * Author: Mike Flynn, C.N.Gann Technology Group, LLC
+ * Author URI: http://cngann.com
+ * License: GPL2
+ */
+	function id_exists($id){ return $wpdb->get_var("SELECT count(*) FROM {$wpdb->posts} WHERE ID = '{$id}'") ? true : false; }
+
+	$included = array();
+
+	add_shortcode('include', function($atts){
+		global $included,$wpdb,$post;
+		$included[get_the_ID()] = true;
+		extract( shortcode_atts( array( 'id' => false, 'slug' => false, 'show_title' => false, 'title_wrapper_elem' => 'h2', 'title_wrapper_class' => '', 'recursion' => 'weak' ), $atts, 'include' ) );
+		$st = $show_title; $twe = $title_wrapper_elem; $twc = $title_wrapper_class; $st = $show_title; $re = $recursion;
+		if($id) { if( ! id_exists( $id )) return ""; }
+		else if(!$id && !$slug) return "";
+		else if($slug) $id = $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_name = '{$slug}'");
+		if(!$id) return "";
+		if(empty($included[$id])) $included[$id] = '';
+		if($included[$id] === true) return "";
+		$included[$id] = true;
+		$op = clone $post;
+		$post = get_post($id);
+		setup_postdata($post);
+		$c = $post->post_content;
+		$t = $post->post_title;
+		$r = ($st ? ( $twe ? "<" . $twe . ($twc ? " class=\"" . $twc . "\" " : "") . ">" . $t . "</".$twe.">" : $t ) : "") . do_shortcode( wpautop( (strtolower($re) == "strict" ? preg_replace("/\[include[^\]]*\]/im","",$c) : $c), true ) );
+		$post = clone $op;
+		setup_postdata($post);
+		$included[$id] = false;
+		return $r;
+	});
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Include ===
+Contributors: mflynn
+Tags: shortcodes, posts, pages, the loop, include, include other post, include other pages,
+Requires at least: 3.8.1
+Tested up to: 3.8.1
+Stable tag: 1.1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A shortcode that includes other posts / pages with no nesting of the shortcode, to allow for multiple pages to call each other so that they display their chunks in different orders.
+
+== Description ==
+
+A shortcode that includes other posts / pages with no nesting of the shortcode, to allow for multiple pages to call each other so that they display their chunks in different orders.
+
+**Requires PHP version 5.3.0 or Higher**
+
+`[include id="XXX" show_title="true" title_wrapper="h2" recursion="weak" ]`
+
+Recursion options:
+* strict: only show first page, do not run `[include]` if it's included
+* weak: only filter out shortcodes with the same id as the current shortcode to prevent infinate loops
+
+== Installation ==
+
+1. Upload `include-shortcode` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
