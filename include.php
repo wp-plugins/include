dbdf652913d156a973f8fd133686aa0a6204d76d
mflynn <mflynn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-24 20:36:29 +0000

    Fixing recursion bug
    git-svn-id: https://plugins.svn.wordpress.org/include/trunk@864286 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include.php
+++ include.php
@@ -3,19 +3,20 @@
  * Plugin Name: Include
  * Plugin URI: http://wordpress.org/plugins/include/
  * Description: Include a page, post, activity, or other query-object into another.
- * Version: 1.4
+ * Version: 1.5
  * Author: mflynn, cngann
  * Author URI: http://cngann.com
  * License: GPL2
  */
 	function id_exists($id){ return $wpdb->get_var("SELECT count(*) FROM {$wpdb->posts} WHERE ID = '{$id}'") ? true : false; }
 
-	$included = array( get_the_ID() => true );
+	$included = array(  );
 
 	add_shortcode('include', 'include_shortcode');
 
 	function include_shortcode ($atts, $content){
 		global $included,$wpdb,$post;
+		$included[get_the_ID()] = true;
 		extract( shortcode_atts( array( 'id' => false, 'slug' => false, 'show_title' => false, 'title_wrapper_elem' => 'h2', 'title_wrapper_class' => '', 'recursion' => 'weak', 'hr' => 'n' ), $atts, 'include' ) ); $st = $show_title; $twe = $title_wrapper_elem; $twc = $title_wrapper_class; $st = $show_title; $re = $recursion; $hr = strtolower($hr) == 'y';
 		if($id &&  ! id_exists( $id )) return "";  else if(!$id && !$slug) return ""; else if($slug) $id = $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_name = '{$slug}'");
 		if(!$id) return ""; if(empty($included[$id])) $included[$id] = ''; if($included[$id] === true) return ""; $included[$id] = true;
