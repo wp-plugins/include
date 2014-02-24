8fab9f6028bf757eef2e3016ec7ccf226ee4f3e8
mflynn <mflynn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-24 13:01:34 +0000

    New and Improved Documentation
    git-svn-id: https://plugins.svn.wordpress.org/include/tags/1.4@864044 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include.php
+++ include.php
@@ -1,38 +1,27 @@
 <?php
 /**
  * Plugin Name: Include
- * Plugin URI:http://www.cngann.com/
+ * Plugin URI: http://wordpress.org/plugins/include/
  * Description: Include a page, post, activity, or other query-object into another.
- * Version: 1.1.1
- * Author: Mike Flynn, C.N.Gann Technology Group, LLC
+ * Version: 1.4
+ * Author: mflynn, cngann
  * Author URI: http://cngann.com
  * License: GPL2
  */
 	function id_exists($id){ return $wpdb->get_var("SELECT count(*) FROM {$wpdb->posts} WHERE ID = '{$id}'") ? true : false; }
 
-	$included = array();
+	$included = array( get_the_ID() => true );
 
-	add_shortcode('include', function($atts){
+	add_shortcode('include', 'include_shortcode');
+
+	function include_shortcode ($atts, $content){
 		global $included,$wpdb,$post;
-		$included[get_the_ID()] = true;
-		extract( shortcode_atts( array( 'id' => false, 'slug' => false, 'show_title' => false, 'title_wrapper_elem' => 'h2', 'title_wrapper_class' => '', 'recursion' => 'weak' ), $atts, 'include' ) );
-		$st = $show_title; $twe = $title_wrapper_elem; $twc = $title_wrapper_class; $st = $show_title; $re = $recursion;
-		if($id) { if( ! id_exists( $id )) return ""; }
-		else if(!$id && !$slug) return "";
-		else if($slug) $id = $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_name = '{$slug}'");
-		if(!$id) return "";
-		if(empty($included[$id])) $included[$id] = '';
-		if($included[$id] === true) return "";
-		$included[$id] = true;
-		$op = clone $post;
-		$post = get_post($id);
-		setup_postdata($post);
-		$c = $post->post_content;
-		$t = $post->post_title;
-		$r = ($st ? ( $twe ? "<" . $twe . ($twc ? " class=\"" . $twc . "\" " : "") . ">" . $t . "</".$twe.">" : $t ) : "") . do_shortcode( wpautop( (strtolower($re) == "strict" ? preg_replace("/\[include[^\]]*\]/im","",$c) : $c), true ) );
-		$post = clone $op;
-		setup_postdata($post);
-		$included[$id] = false;
+		extract( shortcode_atts( array( 'id' => false, 'slug' => false, 'show_title' => false, 'title_wrapper_elem' => 'h2', 'title_wrapper_class' => '', 'recursion' => 'weak', 'hr' => 'n' ), $atts, 'include' ) ); $st = $show_title; $twe = $title_wrapper_elem; $twc = $title_wrapper_class; $st = $show_title; $re = $recursion; $hr = strtolower($hr) == 'y';
+		if($id &&  ! id_exists( $id )) return "";  else if(!$id && !$slug) return ""; else if($slug) $id = $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_name = '{$slug}'");
+		if(!$id) return ""; if(empty($included[$id])) $included[$id] = ''; if($included[$id] === true) return ""; $included[$id] = true;
+		$op = clone $post; $post = get_post($id); setup_postdata($post); $c = $post->post_content; $t = $post->post_title;
+		$r = ($hr ? "<hr />" : "") . ($st ? ( $twe ? "<" . $twe . ($twc ? " class=\"" . $twc . "\" " : "") . ">" . $t . "</".$twe.">" : $t ) : "") . do_shortcode( wpautop( (strtolower($re) == "strict" ? preg_replace("/\[include[^\]]*\]/im","",$c) : $c), true ) );
+		$post = clone $op; setup_postdata($post); unset($included[$id]);
 		return $r;
-	});
+	}
 ?>
